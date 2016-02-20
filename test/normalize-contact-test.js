dfb9ed3345d69ebc803cb523eaa59d37f612cd09
maccyber <jonas.enge@gmail.com>
2016-02-20 17:29:44 +0100

    Added test for empty middlename

--- test/normalize-contact-test.js
+++ test/normalize-contact-test.js
@@ -5,7 +5,12 @@ var normalizeContact = require('../index')
 var dsf = require('./data/dsf.json')
 var expectedResult = require('./data/contact.json')
 
+var dsfEmptyMiddleName = require('./data/dsf-empty-middlename.json')
+var expectedResultEmptyMiddlename = require('./data/contact-empty-middlename.json')
+
 tap.equal(JSON.stringify(expectedResult), JSON.stringify(normalizeContact(dsf)), 'It returns expected result')
+
+tap.equal(JSON.stringify(expectedResultEmptyMiddlename), JSON.stringify(normalizeContact(dsfEmptyMiddleName)), 'It returns expected result')
 
 tap.throws(
   function () {
